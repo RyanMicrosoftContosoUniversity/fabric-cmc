e4bcd9d73c559cd63e9c909b0d5845b9d12778d5
ryanMicrosoftTest <admin@mngenvmcap372892.onmicrosoft.com>
2025-06-17 09:57:10 -0400

    Committing 1 item from workspace 9d04a637-9cd3-41f2-8c4d-e3e1e18041a5

---
+++ fabric_items/cmc-poc-nb.Notebook/notebook-content.py
@@ -0,0 +1,87 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# MARKDOWN ********************
+
+# # Central Management Console Back End
+# Back End Connectivity for Central Management Console
+
+# CELL ********************
+
+import requests
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.keyvault.secrets import SecretClient
+import os
+import notebookutils
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+def get_api_token_via_akv(kv_uri:str, client_id_secret:str, tenant_id_secret:str, client_secret_name:str)->str:
+    """
+    Function to retrieve an api token used to authenticate with Microsoft Fabric APIs
+
+    kv_uri:str: The uri of the azure key vault
+    client_id_secret:str: The name of the key used to store the value for the client id in the akv
+    tenant_id_secret:str: The name of the key used to store the value for the tenant id in the akv
+    client_secret_name:str: The name of the key used to store the value for the client secret in the akv
+
+    """
+    client_id = notebookutils.credentials.getSecret(kv_uri, client_id_secret)
+    tenant_id = notebookutils.credentials.getSecret(kv_uri, tenant_id_secret)
+    client_secret = notebookutils.credentials.getSecret(kv_uri, client_secret_name)
+
+    credential = ClientSecretCredential(tenant_id, client_id, client_secret)
+    scope = 'https://analysis.windows.net/powerbi/api/.default'
+    token = credential.get_token(scope).token
+
+    return token
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# get oauth token
+kv_uri = 'https://kvfabricprodeus2rh.vault.azure.net/'
+client_id_secret = 'fuam-spn-client-id'
+tenant_id_secret = 'fuam-spn-tenant-id'
+client_secret_name = 'fuam-spn-secret'
+
+
+token = get_api_token_via_akv(kv_uri, client_id_secret, tenant_id_secret, client_secret_name)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
